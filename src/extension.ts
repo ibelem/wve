4807aaad999842bf83124328c86c00455d9a9522
Belem Zhang <belem.zhang@intel.com>
2022-05-11 16:53:36 +0800

    init

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ProgressLocation } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,11 +17,62 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('webnizer.ui', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Webnizer Started!');
-		vscode.window.showInformationMessage('10 9 8 7 6');
+		vscode.window.showInformationMessage('Webnizer - Hello!');
+	});
+
+	let disposable2 = vscode.commands.registerCommand('webnizer.build', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Webnizer - Starting to build your project to Wasm');
+		vscode.window.showWarningMessage('Webnizer - Warning');
+		vscode.window.showErrorMessage('Webnizer - Error');
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
+
+	const outputChannel = vscode.window.createOutputChannel(`Webnier`);
+	outputChannel.appendLine('Webnizer Build Log');
+	outputChannel.show(true);
+
+	setTimeout(() => {
+			outputChannel.appendLine('Test1');
+			outputChannel.appendLine('Test2');
+			outputChannel.appendLine('Test3');
+	}, 5000);
+
+	vscode.window.withProgress({
+		location: ProgressLocation.Notification,
+		title: "I am long running!",
+		cancellable: true
+	}, (progress, token) => {
+		token.onCancellationRequested(() => {
+			console.log("User canceled the long running operation");
+		});
+
+		progress.report({ increment: 0 });
+
+		setTimeout(() => {
+			progress.report({ increment: 10, message: "I am long running! - still going..." });
+		}, 1000);
+
+		setTimeout(() => {
+			progress.report({ increment: 40, message: "I am long running! - still going even more..." });
+		}, 2000);
+
+		setTimeout(() => {
+			progress.report({ increment: 50, message: "I am long running! - almost there..." });
+		}, 3000);
+
+		const p = new Promise<void>(resolve => {
+			setTimeout(() => {
+				resolve();
+			}, 5000);
+		});
+
+		return p;
+	});
+
 }
 
 // this method is called when your extension is deactivated
